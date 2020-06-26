bcfe13fca1dd406eab4ac863c9435e8c4e2967df
elisaes <alisa260785@gmail.com>
2020-06-26 16:15:27 +0800

    creating the house

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const player = {
   live: 10,
   origin: { x: 300, y: 300 },
 };
+const house = {};
 
 let bulletId = 0;
 let walk = false;
@@ -48,14 +49,8 @@ class Enemy {
   }
 
   update() {
-    this.x = this.x + 1;
-    //console.log("this.x", this.x);
-    this.y += Math.sin(this.x / 10) * 10 + this.deltay;
-    //console.log("this.y", this.y);
-    // if (this.x > 600 || this.y > 600 || this.y < 0) {
-    //   this.x = 0;
-    //   this.y = 0;
-    // }
+    this.x = this.x + 0.5;
+    this.y += Math.sin(this.x / 10) * 5 + this.deltay;
   }
 
   draw() {
@@ -80,7 +75,6 @@ class Bullet {
     this.direction = direction;
   }
   update() {
-    //console.log(this.x,this.y)
     if (this.direction === "right") {
       this.x = this.x + 10;
     }
@@ -113,8 +107,6 @@ class Bullet {
 
   shootingEnemies() {
     for (const key in enemies) {
-      //    console.log("this.x",this.x);
-      //    console.log("enemies[key].x",enemies[key].x);
       if (
         Math.abs(this.x - enemies[key].x) <= enemies[key].width &&
         Math.abs(this.y - enemies[key].y) <= enemies[key].width
@@ -126,15 +118,34 @@ class Bullet {
 }
 
 class Ornament {
-  constructor(x, y, width, height, img) {
+  constructor(x, y, width, height, color) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
-    this.img = img;
+    this.color = color;
   }
   draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    let x = this.x - player.x + player.origin.x;
+    let y = this.y - player.y + player.origin.y;
+
+    ctx.fillStyle = this.color;
+    ctx.fillRect(x, y, this.width, this.height);
+  }
+}
+
+class Img extends Ornament {
+  constructor(x, y, width, height, img) {
+    super(x, y, width, height);
+    this.img = img;
+  }
+
+  draw() {
+    let x = this.x - player.x + player.origin.x;
+    let y = this.y - player.y + player.origin.y;
+
+    // console.log(x,y)
+    ctx.drawImage(this.img, x, y, this.width, this.height);
   }
 }
 //------------------------PLayer------------------------------------//
@@ -176,6 +187,28 @@ const playerMoving = (e) => {
     keys[e.code] = false;
   }
 };
+// const colliding = () => {
+//   console.log("player.x, player.y", player.x, player.y);
+//   //--------top------------------//
+//   if(player.x<=house[5].x && player.y<=house[8].y){
+
+//   }
+//   if (player.x <=  && player.y <= this.y) {
+//     console.log("player.x, player.y", player.x, player.y);
+//     //player.x = this.x;
+//     player.y = this.y - 2;
+//     player.x = this.x - 2;
+//     // console.log("player.x, player.y", player.x, player.y);
+//     // console.log("this.x, this.y", this.x, this.y);
+//   }
+//   if (player.x <= this.x) {
+//     console.log("player.x, player.y", player.x, player.y);
+//     //player.x = this.x;
+//     player.x = this.x;
+//     // console.log("player.x, player.y", player.x, player.y);
+//     // console.log("this.x, this.y", this.x, this.y);
+//   }
+// };
 
 window.addEventListener("keydown", playerMoving);
 window.addEventListener("keyup", playerMoving);
@@ -199,17 +232,146 @@ const callingBalls = (e) => {
     bulletObj[bulletId] = bullet;
     //console.log("id", bulletId);
   }
-  console.log(bulletObj);
+  //console.log(bulletObj);
 };
 
+const callingHouse = () => {
+  const xArr = [
+    0,
+    0,
+    1000,
+    0,
+    400,
+    400,
+    250,
+    0,
+    400,
+    400,
+    250,
+    0,
+    400,
+    400,
+    700,
+    650,
+    400,
+    700,
+    700,
+    700,
+    900,
+    650,
+    700,
+    900,
+  ];
+  const yArr = [
+    0,
+    1000,
+    0,
+    0,
+    850,
+    600,
+    600,
+    600,
+    400,
+    200,
+    200,
+    200,
+    150,
+    0,
+    700,
+    600,
+    600,
+    250,
+    0,
+    600,
+    600,
+    400,
+    400,
+    400,
+  ];
+  const wArr = [
+    10,
+    1000,
+    10,
+    1000,
+    10,
+    10,
+    150,
+    150,
+    150,
+    10,
+    150,
+    150,
+    10,
+    10,
+    10,
+    100,
+    100,
+    10,
+    10,
+    100,
+    100,
+    150,
+    100,
+    100,
+  ];
+  const hArr = [
+    1000,
+    10,
+    1000,
+    10,
+    150,
+    150,
+    10,
+    10,
+    10,
+    200,
+    10,
+    10,
+    50,
+    50,
+    300,
+    10,
+    10,
+    150,
+    150,
+    10,
+    10,
+    10,
+    10,
+    10,
+  ];
+  console.log(xArr.length,yArr.length,wArr.length,hArr.length)
+  for (let i = 0; i < xArr.length; i++) {
+    house[`${i}`] = new Ornament(xArr[i], yArr[i], wArr[i], hArr[i], "pink");
+  }
+};
+callingHouse();
+
+// const bottom = new Ornament(0, 500, 500, 10, "pink");
+// const left = new Ornament(0, 0, 10, 500, "pink");
+// const tup = new Ornament(0, 0, 500, 10, "red");
+// const right = new Ornament(490, 0, 10, 500, "red");
+
+//const left = new Ornament(10,1000,10,1000,"pink");
+// house["bottom"] = bottom;
+// house["left"] = left;
+// house["tup"] = tup;
+// house["right"] = right;
+
+//house[left]=left;
+
+//console.log("house", house);
 window.addEventListener("keydown", callingBalls);
 
 //---------------------------Set Interval---------------------------//
-
+let time = Date.now();
 const loop = () => {
   if (!gameStatus) {
     return;
   }
+  const diff = Date.now() - time;
+  //  console.log(diff);
+  time = Date.now();
   const enemiesArr = Object.values(enemies);
   const bulletArr = Object.values(bulletObj);
   // console.log(bulletArr)
@@ -225,10 +387,12 @@ const loop = () => {
     bullet.draw();
     bullet.shootingEnemies();
   });
-  //console.log(enemies);
-
+  for (const key in house) {
+    house[key].draw();
+  }
   moving();
   makingPlayer();
+  console.log(player.x, player.y);
 };
 
-setInterval(loop, 44);
+setInterval(loop, 16);
