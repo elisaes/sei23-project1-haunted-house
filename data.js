2684cd7418d3a8e11ef59b4a02b6110ba497f3e7
elisaes <alisa260785@gmail.com>
2020-06-28 20:58:59 +0800

    adding furniture

---
+++ data.js
@@ -0,0 +1,30 @@
+//-ornaments-----//
+const imgX = [930, 750, 630, 550, 415, 410, 10, 175,10,170,325,680,100,10,100,200,570,450,480,410,410,710];
+const imgY = [615, 800, 900, 920, 970, 610, 710, 920,910,750,610,740,300,70,14,10,10,10,350,230,150,10];
+const imgW = [60, 200, 70, 60, 50, 70, 70, 150,100,100,70,20,200,60,50,150,130,120,70,70,70,100];
+const imgH = [40, 200, 100, 70, 30, 50, 150, 70,100,100,70,100,200,60,50,70,50,50,50,170,80,50];
+const imgUrl = [
+  "./img/comoda.png",
+  "./img/bed1.png",
+  "./img/table2.png",
+  "./img/sofa2.png",
+  "./img/books1.png",
+  "./img/comoda.png",
+  "./img/sofa4.png",
+  "./img/sofa5.png",
+  "./img/plant1.png",
+  "./img/cofeetable1.png",
+  "./img/redchair.png",
+  "./img/tv2.png",
+  "./img/tableDining1.png",
+  "./img/toilet.png",
+  "./img/lavabo.png",
+  "./img/bath.png",
+  "./img/sink1.png",
+  "./img/table3.png",
+  "./img/stove1.png",
+  "./img/table5.png",
+  "./img/washing1.png",
+  "./img/coach1.png"
+
+];
--- script.js
+++ script.js
@@ -410,10 +410,10 @@ for (let i = 0; i < xArr.length; i++) {
 console.log("house", house);
 
 //-ornaments-----//
-const imgX = [930, 750, 630, 550, 415, 410, 10, 175,10,170,325,680,100,10];
-const imgY = [615, 800, 900, 920, 970, 610, 710, 920,910,750,610,740,300,70];
-const imgW = [60, 200, 70, 60, 50, 70, 70, 150,100,100,70,20,200,60];
-const imgH = [40, 200, 100, 70, 30, 50, 150, 70,100,100,70,100,200,60];
+const imgX = [930, 750, 630, 550, 415, 410, 10, 175,10,170,325,680,100,10,100,200,570,450,480,410,410,710];
+const imgY = [615, 800, 900, 920, 970, 610, 710, 920,910,750,610,740,300,70,14,10,10,10,350,230,150,10];
+const imgW = [60, 200, 70, 60, 50, 70, 70, 150,100,100,70,20,200,60,50,150,130,120,70,70,70,150];
+const imgH = [40, 200, 100, 70, 30, 50, 150, 70,100,100,70,100,200,60,50,70,50,50,50,170,80,70];
 const imgUrl = [
   "./img/comoda.png",
   "./img/bed1.png",
@@ -428,9 +428,18 @@ const imgUrl = [
   "./img/redchair.png",
   "./img/tv2.png",
   "./img/tableDining1.png",
-  "./img/toilet.png"
+  "./img/toilet.png",
+  "./img/lavabo.png",
+  "./img/bath.png",
+  "./img/sink1.png",
+  "./img/table3.png",
+  "./img/stove1.png",
+  "./img/table5.png",
+  "./img/washing1.png",
+  "./img/coach1.png"
+
 ];
-for (let i = 0; i < 14; i++) {
+for (let i = 0; i < imgX.length; i++) {
   imgObj[i] = new Img(imgX[i], imgY[i], imgW[i], imgH[i], null, imgUrl[i]);
 }
 console.log("imgObj", imgObj);
